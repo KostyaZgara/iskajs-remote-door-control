f887041626e9db52436afc5cd6979d475b28316a
Kostya Zgara <k.zgara@seedium.io>
2021-03-10 12:55:16 +0200

    fix opening and closing the gate

--- index.js
+++ index.js
@@ -1,20 +1,41 @@
-const insideReceiver = require('@amperka/ir-receiver').connect(P2);
-const outsideReceiver = require('@amperka/ir-receiver').connect(P3);
+const insideReceiver = require('@amperka/ir-receiver').connect(P0);
+const outsideReceiver = require('@amperka/ir-receiver').connect(P1);
 
 // power switches
-const openGateKeyPin = P0;
-const closeGateKeyPin = P1;
+const openGateKeyPin = P10;
+const closeGateKeyPin = P8;
 
 // audio signals
-const mainAudioSignalPin = P4;
+const mainAudioSignalPin = P5;
+
+/*
+* Pins to related numbers
+* P0 = 11
+* P1 = 10
+* P2 = 6
+* P3 = 7
+* P4 = 3
+* P5 = 1
+* P6 = 0
+* P7 = 2
+* P8 = 6
+* P9 = 7
+* P10 = 8
+* P11 = 9
+* P12 = 8
+* P13 = 10
+*
+* Grouped pins that relates to the same number
+* P0, (P1-P13), (P2-P8), (P3-P9), P4, P5, P6, P7, (P10-P12), P11
+* */
 
 // barrier indicators
-const lockRightBarrier = P8;
-const lockLeftBarrier = P9;
-const bottomRightBarrier = P10;
+const lockRightBarrier = P2;
+const lockLeftBarrier = P3;
+const topRightBarrier = P6;
+const topLeftBarrier = P7;
 const bottomLeftBarrier = P11;
-const topRightBarrier = P12;
-const topLeftBarrier = P13;
+const bottomRightBarrier = P12;
 
 // control buttons
 const powerButton = 0xfd00ff;
@@ -28,7 +49,7 @@ function beep() {
   mainAudioSignalPin.reset();
   setTimeout(() => {
     mainAudioSignalPin.set();
-  }, 5000);
+  }, 1000);
 }
 
 function isGateLocked() {
@@ -39,16 +60,13 @@ function isGateLocked() {
 function openGatePowerSwitch(keyPin) {
   if (isGateLocked()) {
     beep();
-    return;
+    return false;
   }
   keyPin.set();
+  return true;
 }
 
 function closeGatePowerSwitch(keyPin) {
-  if (isGateLocked()) {
-    beep();
-    return;
-  }
   keyPin.reset();
 }
 
@@ -85,52 +103,69 @@ ButtonController.prototype._updateReleaseTimeout = function (timeout) {
 };
 
 const GateStateController = function () {
-  this._isClosed = true;
-  this._isOpened = false;
-  this._inProgress = false;
+  this.reset();
+  const self = this;
+  this._setWatch([topRightBarrier], function () {
+    if (!self._isOpening) {
+      return;
+    }
+    closeGatePowerSwitch(openGateKeyPin);
+    self._isOpened = true;
+    self._isOpening = false;
+  });
+  this._setWatch([bottomRightBarrier], function () {
+    if (!self._isClosing) {
+      return;
+    }
+    closeGatePowerSwitch(closeGateKeyPin);
+    self._isClosed = true;
+    self._isClosing = false;
+  });
 };
 
 GateStateController.prototype.switchState = function () {
-  if (this._isClosed && !this._inProgress) {
+  const inProgress = this._isClosing || this._isOpening;
+  if (this._isClosed && !inProgress) {
     this._openGate();
-  } else if (this._isOpened && !this._inProgress) {
+  } else if (this._isOpened && !inProgress) {
     this._closeGate();
   }
 };
 
+GateStateController.prototype.reset = function () {
+  this._isClosed = true;
+  this._isClosing = false;
+  this._isOpened = false;
+  this._isOpening = false;
+};
+
 GateStateController.prototype._openGate = function () {
-  this._inProgress = true;
-  this._isClosed = false;
-  const self = this;
-  openGatePowerSwitch(openGateKeyPin);
-  this._setWatch([topLeftBarrier, topRightBarrier], function (e) {
-    closeGatePowerSwitch(openGateKeyPin);
-    self._inProgress = false;
-    self._isOpened = true;
-  });
+  const result = openGatePowerSwitch(openGateKeyPin);
+  if (result) {
+    this._isOpening = true;
+    this._isClosed = false;
+  }
 };
 
 GateStateController.prototype._closeGate = function () {
-  this._inProgress = true;
-  this._isOpened = false;
-  const self = this;
-  openGatePowerSwitch(closeGateKeyPin);
-  this._setWatch([bottomLeftBarrier, bottomRightBarrier], function (e) {
-    closeGatePowerSwitch(closeGateKeyPin);
-    self._inProgress = false;
-    self._isClosed = true;
-  });
+  const result = openGatePowerSwitch(closeGateKeyPin);
+  if (result) {
+    this._isClosing = true;
+    this._isOpened = false;
+  }
 };
 
 GateStateController.prototype._setWatch = function (pins, cb) {
-  return pins.map(
+  pins.forEach(
     (pin) => setWatch(cb, pin, {
-      repeat: false,
+      repeat: true,
       edge: 'falling',
-      debounce: 10,
+      debounce: 10
     })
   );
 };
+
+const gateStateController = new GateStateController();
 
 // Handlers
 function startGateUpHandler() {
@@ -161,17 +196,13 @@ function startPowerOffHandler() {
   powerButtonController.on('press', function () {
     closeGatePowerSwitch(openGateKeyPin);
     closeGatePowerSwitch(closeGateKeyPin);
-  });
-
-  powerButtonController.on('hold', function () {
-    closeGatePowerSwitch(openGateKeyPin);
-    closeGatePowerSwitch(closeGateKeyPin);
+    gateStateController.reset();
+    mainAudioSignalPin.set();
   });
 }
 
 function startSwitchGateStateHandler() {
   const switchGateStateButtonController = new ButtonController((code) => code === switchGateStateButton);
-  const gateStateController = new GateStateController();
 
   switchGateStateButtonController.on('press', function () {
     gateStateController.switchState();
